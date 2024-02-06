8ad516035638e5a942ea5b5fec1a42debf9f2116
Nils Garvik <ngarvik@FRL2365.local>
2024-02-06 11:25:31 +0100

    Add project

--- storage.tf
+++ storage.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "static-site" {
   name          = "test-tf-cloud-build"
   location      = "EU"
   force_destroy = true
+  project       = var.project
 
   uniform_bucket_level_access = true
 
